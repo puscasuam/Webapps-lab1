55d4521f0d3a7a025b2d191ade21ab5e8dffa284
Ana <puscasu.a.m@gmail.com>
2020-05-10 12:37:23 +0300

    Add project files.

---
+++ Webapps lab1/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webapps_lab1.Models
+{
+    public class User
+    {
+    }
+}
---
+++ Webapps lab1/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webapps_lab1.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public float Salary { get; set; }
+    }
+}
